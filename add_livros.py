aa75d355e1d251c060132bf839ae5a471f2b85a3
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-22 15:13:00 -0300

    add livro

---
+++ add_livros.py
@@ -0,0 +1,10 @@
+import sqlite3
+conn = sqlite3.connect("biblioteca.db")
+
+#opçâo de adicionar livros
+
+def add_livros():
+    #id =
+    titulo = print("Digite o título do livro: ")
+    autor = print("Digite o nome do autor(a): ")
+    
